b796f5b0aaa76008cf81e53f8fc5ba830874714f
Jeremy <fractallian@gmail.com>
2014-06-23 18:20:18 -0700

    model.save only updates changed data
    added deep-diff dependency

--- package.js
+++ package.js
@@ -4,10 +4,10 @@ Package.describe({
 
 Package.on_use(function (api, where) {
   api.use(["underscore"], ["client", "server"]);
-  api.add_files(['lib/model.js', 'lib/relations.js', 'graviton.js'], ['client', 'server']);
+  api.use(['deep-diff'], ['client', 'server']);
+  api.add_files(['lib/model.js', 'lib/relations.js', 'graviton.js', 'lib/mongo-diff.js'], ['client', 'server']);
 
   if (typeof api.export !== 'undefined') {
-    // api.export("Model", ["client", "server"]);
     api.export("Graviton", ["client", "server"]);
   }
 });
@@ -15,7 +15,7 @@ Package.on_use(function (api, where) {
 Package.on_test(function (api) {
   api.use(['graviton', 'tinytest', 'test-helpers']);
 
-  api.add_files('test/graviton-test.js', ['client', 'server']);
+  api.add_files(['test/graviton-test.js', 'test/mongo-diff-test.js'], ['client', 'server']);
 });
 
 

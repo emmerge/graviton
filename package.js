f2241f14a9ed9e3c544c2fcf76edf47cb20874fd
mhwheeler <mhwheeler@gmail.com>
2015-01-08 22:54:23 -0800

    fix test api.use

--- package.js
+++ package.js
@@ -21,7 +21,7 @@ Package.on_use(function (api, where) {
 });
 
 Package.on_test(function (api) {
-  api.use(['underscore', 'tinytest', 'test-helpers']);
+  api.use(['underscore', 'emmerge:graviton', 'tinytest', 'test-helpers']);
 
   api.add_files([
     'test/test-helpers.js',

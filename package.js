94873d9b6239823e3bc9e29a105737be47909c72
mhwheeler <mhwheeler@gmail.com>
2015-04-21 16:41:14 -0700

    vbump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'emmerge:graviton',
   summary: "Transform collection records into Models and define and use relationships between Meteor collections",
   git: "https://github.com/emmerge/graviton",
-  version: '0.0.11'
+  version: '0.0.12'
 });
 
 Package.on_use(function (api, where) {

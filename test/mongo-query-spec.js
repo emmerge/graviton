8150a5af6dbf05b3cdefa888794dcd653916fb41
Jeremy <fractallian@gmail.com>
2015-11-25 12:26:23 -0800

    relax key conflict rules a bit

--- test/mongo-query-spec.js
+++ test/mongo-query-spec.js
@@ -30,6 +30,20 @@ describe('Graviton.MongoModifier', function() {
     });
   });
 
+  describe('keysConflict', function() {
+    var conflict = Graviton.MongoModifier.keysConflict;
+
+    it('should return true if one key contains another', function() {
+      expect(conflict('a', 'a')).toBe(false); // same
+      expect(conflict('a.b.c', 'a.b.d')).toBe(false); // same length
+      expect(conflict('a.b.c.d', 'a.b.d.f.g')).toBe(false); // different length
+      expect(conflict('a.b.d.f.g', 'a.b.c.d')).toBe(false);
+      expect(conflict('a.b', 'a.b.d')).toBe(true);
+      expect(conflict('a.b.c', 'a.b')).toBe(true);
+    });
+
+  });
+
   describe('an instance', function() {
     beforeEach(function() {
       this.mod = new Graviton.MongoModifier();
@@ -60,10 +74,22 @@ describe('Graviton.MongoModifier', function() {
       expect(this.mod.modObject()).toEqual(expected);
     });
 
-    it('should not allow keys that contain another key', function() {
-      this.mod.set({x: {y: 'z'}});
-      var set = {x: 'a'};
-      expect(() => this.mod.set(set)).toThrowError(Error);
+    describe('set', function() {
+      it('should prevent multiple calls that conflict', function() {
+        this.mod.set({x: {y: 'z', m: 'n'}});
+        var set = {x: 'a'};
+        expect(() => this.mod.set(set)).toThrowError(Error);
+
+        set = {x: {y: 'r'}};
+        expect(() => this.mod.set(set)).not.toThrowError();
+      });
+
+      it('should allow overlapping keys that make sense', function() {
+        this.mod.set({x: {y: 'z'}});
+        var set = {x: 'a'};
+        expect(() => this.mod.set(set)).toThrowError(Error);
+      });
     });
+
   });
 });

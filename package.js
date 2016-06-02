c3c8a4ae8e16a9edcde2f0841982b1142227b4a5
Mark Lanett <mark.lanett@gmail.com>
2016-06-02 15:18:09 -0700

    Version bump.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'emmerge:graviton',
   summary: "Transform collection records into Models and define and use relationships between Meteor collections",
   git: "https://github.com/emmerge/graviton",
-  version: '0.0.21'
+  version: '0.0.22'
 });
 
 Package.on_use(function (api, where) {
